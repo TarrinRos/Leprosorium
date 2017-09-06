518093ce4991e70524cbad96881561d842c9b088
TarrinRos <tarindis@gmail.com>
2017-09-06 16:24:06 +0300

    init database, make @db

--- app.rb
+++ app.rb
@@ -3,6 +3,15 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require "sqlite3"
+
+def init_db
+  @db = SQLite3::Database.new './public/leprosorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+end
 
 get '/' do
   erb 'Hello!
