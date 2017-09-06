45d245f62c34b17f180d934f2c71d5b0a0bdab14
TarrinRos <tarindis@gmail.com>
2017-09-06 19:11:53 +0300

    Add details view

--- app.rb
+++ app.rb
@@ -45,3 +45,12 @@ post '/new' do
   redirect to '/'
   erb "You typed #{@content}"
 end
+
+# Returns posts by Id
+get '/details/:post_id' do
+  post_id = params[:post_id]
+
+  results = @db.execute 'select * from Posts where id =?', [post_id]
+  @row = results[0]
+  erb :details
+end
